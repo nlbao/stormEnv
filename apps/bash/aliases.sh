0b6e8167d6629e87d84c0fe77ab9042b3500693b
Bao Nguyen <nlbao@Baos-MBP.frontier.com>
2020-05-24 15:45:51 -0400

    Update bashrc, create a separated alias.

---
+++ apps/bash/aliases.sh
@@ -0,0 +1,71 @@
+# some more ls aliases
+alias ll='ls -alFh'
+# alias ll='ls -lah --color=auto --time-style="+%a %Y-%m-%d %H:%M"'
+
+# Add an "alert" alias for long running commands.  Use like so:
+
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+# my settings
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+get_git_branch() {
+    echo $(parse_git_branch) | sed 's/(//g' | sed 's/)//g'
+}
+
+function gitbr {
+    git branch -D $1
+    git checkout -b $1
+}
+
+    # git log
+GLF1='--oneline --graph --color --decorate --all'
+GLF2="--graph --abbrev-commit --color --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+GLF3="--graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+alias gl='git log -10 '$GLF2
+alias glall='git log '$GLF2
+alias gstt='git status'
+alias gitf='git fetch'
+alias gdiff='git diff'
+alias gadd='git add'
+alias gcom='git commit -m'
+alias gcom_add='git commit --amend'
+alias gpush='git push origin $(get_git_branch)'
+alias gpush_add='git push origin -u +$(get_git_branch)'
+alias gpull='git pull origin $(get_git_branch)'
+
+    # cd
+alias ..='cd ..'
+alias .3='cd ../../../'
+
+    # mem / cpu / gpu info
+alias meminfo='free -m -l -t'
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+alias cpuinfo='lscpu'
+alias gpumeminfo='grep -i --color memory /var/log/Xorg.0.log'
+alias wget='wget -c'  # resume wget by default
+alias hs='history | grep $1'  # history search (use: hs sometext)
+alias svim='sudo vim'  # sudo vim
+alias myip='hostname --ip-address'
+alias myos='cat /etc/os-release; lsb_release -a; uname -r'
+
+    # tmux
+alias retmux='tmux source-file ~/.tmux.conf'
+
+function tmuxall {
+    tmux -at -t all
+}
+
+function tmux_start {
+    tmux new -s "$1" \; send-keys "os $1" 'C-m'
+}
--- apps/bash/bashrc.sh
+++ apps/bash/bashrc.sh
@@ -1,35 +1,113 @@
-# .bashrc
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
 
-# Source global definitions
-if [ -f /etc/bashrc ]; then
-	. /etc/bashrc
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
 fi
 
-# User specific environment
-PATH="$HOME/.local/bin:$HOME/bin:$PATH"
-export PATH
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
 
-# Uncomment the following line if you don't like systemctl's auto-paging feature:
-# export SYSTEMD_PAGER=
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
 
-# User specific aliases and functions
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+    # We have color support; assume it's compliant with Ecma-48
+    # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+    # a case would tend to support setf rather than setaf.)
+    color_prompt=yes
+    else
+    color_prompt=
+    fi
+fi
 
-parse_git_branch() {
-	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
 
-# color ref: https://misc.flogisoft.com/bash/tip_colors_and_formatting
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+source ./aliases.sh
+
+# added by Anaconda2 4.1.0 installer
+export PATH="/home/nlbao/anaconda2/bin:$PATH"
+
+# google_app_engine
+export PATH="/home/nlbao/apps/google_appengine:$PATH"
+
 TIME='\[\033[01;31m\]\t';
-USERNAME='\[\033[01;32m\]\u';
-HOST='\[\033[02;34m\]\h';
-LOCATION='\[\033[01;96m\]`pwd | sed "s#\(/[^/]\{1,\}/[^/]\{1,\}/[^/]\{1,\}/\).*\(/[^/]\{1,\}/[^/]\{1,\}\)/\{0,1\}#\1_\2#g"`';
-BRANCH='\[\033[00;33m\]$(parse_git_branch)';
+# TIME='\t';
+# USERNAME='\[\033[01;32m\]\u';
+USERNAME='\u';
+# HOST='\[\033[02;36m\]\h';
+# LOCATION='\[\033[01;34m\]`pwd | sed "s#\(/[^/]\{1,\}/[^/]\{1,\}/[^/]\{1,\}/\).*\(/[^/]\{1,\}/[^/]\{1,\}\)/\{0,1\}#\1_\2#g"`';
+# BRANCH='\[\033[00;33m\]$(parse_git_branch)';
 PS1=$TIME' '$USERNAME' '$HOST' '$LOCATION''$BRANCH'\[\033[00m\] \$ ';
+# source /etc/profile.d/rvm.sh
 
-alias py='python3'
+# my aliases
+alias prm=". ~/tools/prm/prm.sh"
 
-#### Citrix Receiver ####
-alias citrix='LD_LIBRARY_PATH=/home/nlbao/libs/usr/lib64 /opt/Citrix/ICAClient/wfica -icaroot /opt/Citrix/ICAClient /home/nlbao/Downloads/citrix/mlp.ica'
+    # ls
+alias ls='ls --color'
+alias ll='ls -lah --color=auto --time-style="+%a %Y-%m-%d %H:%M"'
+LS_COLORS='di=1:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=35:*.rpm=90'
+export LS_COLORS
 
-#### tmux ####
-alias tmuxall="tmux a -t all"
