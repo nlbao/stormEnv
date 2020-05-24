9d5549c2898a8981d8ee205e7753db36a95e746c
Bao Nguyen <nlbao95@gmail.com>
2020-05-24 15:57:36 -0400

    Add brew and zsh.

---
+++ apps/brew/setup.sh
@@ -0,0 +1,24 @@
+brew install fontconfig
+brew install wget
+
+
+# wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.
+
+
+
+# A CA file has been bootstrapped using certificates from the system
+# keychain. To add additional certificates, place .pem files in
+#   /usr/local/etc/openssl@1.1/certs
+
+# and run
+#   /usr/local/opt/openssl@1.1/bin/c_rehash
+
+# openssl@1.1 is keg-only, which means it was not symlinked into /usr/local,
+# because macOS provides LibreSSL.
+
+# If you need to have openssl@1.1 first in your PATH run:
+#   echo 'export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"' >> ~/.zshrc
+
+# For compilers to find openssl@1.1 you may need to set:
+#   export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+#   export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
---
+++ apps/zsh/setup.sh
@@ -0,0 +1,22 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# install powerline fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# another font
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.local/share/fonts/
+mv PowerlineSymbols.otf ~/.local/share/fonts/
+fc-cache -vf ~/.local/share/fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+# settings
+# change in preferences > display > text > ANSI Colors (customise based on your vision)
+# Source Code Pro is okay
