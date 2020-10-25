4083417d30ee8e2b46ae469fd35bbdf2c34dc0bc
Bao Nguyen <nlbao95@gmail.com>
2020-10-25 16:29:01 +0800

    [vim] Run plugin installation.

--- apps/vim/setup.sh
+++ apps/vim/setup.sh
@@ -6,3 +6,10 @@ ln -s ~/env/apps/vim/vimrc.vim ~/.vimrc
 ls -alh ~/.vimrc
 
 mkdir -p ~/.vim/sessions
+
+# install plugins using vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# vim +PlugInstall +qall  # TODO: fix this. Github Action timeout
+# TODO: add commands for other plugin managers
+
